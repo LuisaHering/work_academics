c24ae30a078fec68bb7961be40ac17bf3866f157
Luisa <lulucabell@hotmail.com>
2019-10-28 19:49:42 -0300

    idk carlos did it

---
+++ SN_Core/Interface/IUser.cs
@@ -0,0 +1,14 @@
+﻿using SN_WebApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SN_Core.Interface
+{
+    public interface IUser
+    {
+        ApplicationUser findUserByEmail(string email); 
+    }
+}
