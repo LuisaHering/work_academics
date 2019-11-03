f825061b236f3bee8520813477834bea75051579
Carlos Henrique <carlos.hrq.rodrigues@gmail.com>
2019-11-02 22:43:10 -0300

    classe usuario

---
+++ Core/Models/AspNetUsers.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Models {
+    public class AspNetUsers {
+        public string Id {
+            get; set;
+        }
+
+        public string Name {
+            get; set;
+        }
+
+        public string University {
+            get; set;
+        }
+
+        public int Biography {
+            get; set;
+        }
+
+        public DateTime StartDate {
+            get; set;
+        }
+
+        public string Email {
+            get; set;
+        }
+
+        public string EmailConfirmed {
+            get; set;
+        }
+
+        public string PasswordHash {
+            get; set;
+        }
+
+        public string SecurityStamp {
+            get; set;
+        }
+
+        public string PhoneNumber {
+            get; set;
+        }
+
+        public bool PhoneNumberConfirmed {
+            get; set;
+        }
+
+        public bool TwoFactorEnabled {
+            get; set;
+        }
+
+        public DateTime LockoutEndDateUtc {
+            get; set;
+        }
+
+        public bool LockoutEnabled {
+            get; set;
+        }
+
+        public int AccessFailedCount {
+            get; set;
+        }
+
+        public string UserName {
+            get; set;
+        }
+
+        public AspNetUsers() {
+
+        }
+    }
+}
