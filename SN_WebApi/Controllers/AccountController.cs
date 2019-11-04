0c1d9d33e70e4b980752ed3f9bc52c642178cfa8
Carlos Henrique <carlos.hrq.rodrigues@gmail.com>
2019-11-04 20:19:28 -0300

    att

--- SN_WebApi/Controllers/AccountController.cs
+++ SN_WebApi/Controllers/AccountController.cs
@@ -196,7 +196,7 @@ namespace SN_WebApi.Controllers {
                 Curso = model.Curso
             };
 
-            var updated = UsersService.UpdateEF2(updatedUser);
+            var updated = UsersService.Update(updatedUser);
 
             if(!updated) {
                 return BadRequest("Erro ao atualizar os dados do usuario");
