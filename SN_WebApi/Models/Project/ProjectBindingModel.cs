cc5de5909338a81ad3dad07456c33448633639a6
carlos henrique <carlos.hrq.rodrigues@gmail.com>
2019-11-12 11:20:27 -0300

    organizando models api

---
+++ SN_WebApi/Models/Project/ProjectBindingModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SN_WebApi.Models.Project {
+    public class ProjectBindingModel {
+
+        public int Id {
+            get; set;
+        }
+
+        public string Titulo {
+            get; set;
+        }
+
+        public string Descricao {
+            get; set;
+        }
+
+        public DateTime DataCriacao {
+            get; set;
+        }
+
+    }
+}
