6f2eb44134550259d88bee20f09d85246dc2b84f
carlos henrique <carlos.hrq.rodrigues@gmail.com>
2019-11-13 20:46:03 -0300

    relacionamento

--- Core/Models/Picture.cs
+++ Core/Models/Picture.cs
@@ -10,7 +10,6 @@ namespace Core.Models
     {
         public int Id { get; set; }
         public string Url { get; set; }
-        public string UserId { get; set; }
         public virtual User User { get; set; }
     }
 }
