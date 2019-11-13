a1e33738dadf2fb79177b90af771d595a5278664
gabriel <gabrielcouto26@gmail.com>
2019-11-13 19:24:35 -0300

    iniciando metodo upload de arquivos

---
+++ SN_WebApi/ServicosExternos/ServidorDeArquivos.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.WindowsAzure.Storage;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+
+namespace SN_WebApi.ServicosExternos
+{
+    public class ServidorDeArquivos
+    {
+        public void UploadDeArquivo(Stream reader, string nomeDoArquivo)
+        {
+            string connectionString = @"DefaultEndpointsProtocol=https;AccountName=gabrielcouto26;AccountKey=GVZpPG9E+BGF1jV8PkMTMOR9gOvEb0wbTQPVlA7Ea+PjbhuZf153uQwv/m5zqgY3kKwf38o9WdiljROtl/+fIg==;EndpointSuffix=core.windows.net";
+
+            CloudStorageAccount cloudStorageAccount = null;
+
+            CloudStorageAccount.TryParse(connectionString, out cloudStorageAccount);
+
+            var cloudBlobClient = cloudStorageAccount.CreateCloudBlobClient();
+
+            var container = cloudBlobClient.GetContainerReference("wa-profile-pics");
+
+            container.CreateIfNotExists();
+
+            var cloudBlockBlob = container.GetBlockBlobReference(nomeDoArquivo);
+
+            cloudBlockBlob.UploadFromStream(reader);
+        }
+
+        public void UploadDeArquivo(FileStream reader, string nomeDoArquivo)
+        {
+            string connectionString = @"DefaultEndpointsProtocol=https;AccountName=gabrielcouto26;AccountKey=GVZpPG9E+BGF1jV8PkMTMOR9gOvEb0wbTQPVlA7Ea+PjbhuZf153uQwv/m5zqgY3kKwf38o9WdiljROtl/+fIg==;EndpointSuffix=core.windows.net";
+
+            CloudStorageAccount cloudStorageAccount = null;
+
+            CloudStorageAccount.TryParse(connectionString, out cloudStorageAccount);
+
+            var cloudBlobClient = cloudStorageAccount.CreateCloudBlobClient();
+
+            var container = cloudBlobClient.GetContainerReference("wa-profile-pics");
+
+            container.CreateIfNotExists();
+
+            var cloudBlockBlob = container.GetBlockBlobReference(nomeDoArquivo);
+
+            cloudBlockBlob.UploadFromStream(reader);
+        }
+    }
+}
