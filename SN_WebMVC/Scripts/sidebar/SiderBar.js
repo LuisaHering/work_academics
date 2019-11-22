74b18902b9dc850701bd1af7bba0720718c143de
gabriel <gabrielcouto26@gmail.com>
2019-11-22 13:58:51 -0300

    criando script para sidebar

---
+++ SN_WebMVC/Scripts/sidebar/SiderBar.js
@@ -0,0 +1,7 @@
+﻿//o que esse script deve fazer:
+
+//- ao clicar no botao left:
+//sidebar deve reduzir, deve receber nova classe(wa-sidebar-hidden)
+
+//- ao clicar no botao right:
+//sidebar deve reaparecer, com display normal (wa-sidebar-full)
