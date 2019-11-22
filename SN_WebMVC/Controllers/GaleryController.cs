aaecfc2bf261b4f9922e239c442ec399cb445837
gabriel <gabrielcouto26@gmail.com>
2019-11-22 18:40:44 -0300

    criando galeria

---
+++ SN_WebMVC/Controllers/GaleryController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SN_WebMVC.Controllers
+{
+    public class GaleryController : Controller
+    {
+        // GET: Galery
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ SN_WebMVC/Models/GaleryViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SN_WebMVC.Models
+{
+    public class GaleryViewModel
+    {
+        public string ImageUrl { get; set; }
+    }
+}
