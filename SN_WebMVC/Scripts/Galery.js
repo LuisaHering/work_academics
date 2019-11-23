0bed818abf8a3e6ae2873075d54006611e270655
gabriel <gabrielcouto26@gmail.com>
2019-11-23 01:33:57 -0300

    iniciando script sidebar

---
+++ SN_WebMVC/Scripts/Galery.js
@@ -0,0 +1,28 @@
+﻿var btn1 = document.getElementById('btn-resize-to-small')
+var btn2 = document.getElementById('btn-resize-to-small')
+
+btn1.addEventListener('click', function () {
+    var small = document.querySelectorAll("wa-sidebar-list-small"); 
+    var full = document.querySelectorAll("wa-sidebar-list-full");
+
+    for (var i = 0; i <= small.length; i++) {
+        small[i].style.display = "block";
+    }
+
+    for (var i = 0; i <= full.length; i++) {
+        full[i].style.display = "none";
+    }
+})
+
+btn1.addEventListener('click', function () {
+    var small = document.querySelectorAll("wa-sidebar-list-small");
+    var full = document.querySelectorAll("wa-sidebar-list-full");
+
+    for (var i = 0; i <= small.length; i++) {
+        small[i].style.display = "none";
+    }
+
+    for (var i = 0; i <= full.length; i++) {
+        full[i].style.display = "block";
+    }
+})
