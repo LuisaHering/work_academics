0566179ae31fbcf553ae78a7cb9a7754635250cb
gabriel <gabrielcouto26@gmail.com>
2019-11-23 22:57:39 -0300

    botando js pra funcionar

--- SN_WebMVC/Scripts/SideBar.js
+++ SN_WebMVC/Scripts/SideBar.js
@@ -1,28 +1,30 @@
 ﻿var btn1 = document.getElementById('btn-resize-to-small')
 var btn2 = document.getElementById('btn-resize-to-full')
 
-btn1.addEventListener('click', function () {
-    var small = document.querySelectorAll(".wa-sidebar-list-small"); 
-    var full = document.querySelectorAll(".wa-sidebar-list-full");
+var small = document.querySelectorAll(".wa-sidebar-list-small");
+var full = document.querySelectorAll(".wa-sidebar-list-full");
 
-    for (var i = 0; i <= small.length; i++) {
+
+
+btn1.addEventListener('click', function () {
+
+    console.log(small);
+    for (var i = 0; i < small.length; i++) {
         small[i].style.display = "block";
     }
 
-    for (var i = 0; i <= full.length; i++) {
+    for (var i = 0; i < full.length; i++) {
         full[i].style.display = "none";
     }
 })
 
 btn2.addEventListener('click', function () {
-    var small = document.querySelectorAll(".wa-sidebar-list-small");
-    var full = document.querySelectorAll(".wa-sidebar-list-full");
 
-    for (var i = 0; i <= small.length; i++) {
+    for (var i = 0; i < small.length; i++) {
         small[i].style.display = "none";
     }
 
-    for (var i = 0; i <= full.length; i++) {
+    for (var i = 0; i < full.length; i++) {
         full[i].style.display = "block";
     }
 })
