a4c35c4e844e7717b31fe09c18cd26e1210ca2f4
gabriel <gabrielcouto26@gmail.com>
2019-11-25 23:56:13 -0300

    sidebar feia mas funcionando

--- SN_WebMVC/Scripts/SideBar.js
+++ SN_WebMVC/Scripts/SideBar.js
@@ -1,20 +1,29 @@
-﻿var btn = document.getElementById('btn-resize-to-small')
+﻿var btn = document.getElementById('btn-resize')
 
-var small = document.querySelectorAll(".wa-sidebar-list-small");
-var full = document.querySelectorAll(".wa-sidebar-list-full");
+var small = document.querySelectorAll(".wa-sidebar-list-small")
+var full = document.querySelectorAll(".wa-sidebar-list-full")
+var waSideBar = document.querySelector(".wa-sidebar")
+var miniLogo = document.getElementById("mini-logo-sidebar")
+
 
 btn.addEventListener('click', function () {
 
-    // full
+    // cresce
     for (var i = 0; i < small.length; i++) {
         if (small[i].style.display == "none") {
             small[i].style.display = "block"
+            waSideBar.style.width = "6em"
+            miniLogo.style.margin = "0"
+            btn.style.marginBottom = "20px"
         } else {
             small[i].style.display = "none"
+            waSideBar.style.width = "12em"
+            miniLogo.style.margin = "20px 35px"
+            btn.style.marginBottom = "0"
         }
     }
 
-    // small
+    // diminui
     for (var i = 0; i < full.length; i++) {
         if (full[i].style.display == "block") {
             full[i].style.display = "none"
@@ -22,4 +31,6 @@ btn.addEventListener('click', function () {
             full[i].style.display = "block"
         }
     }
+
+    
 })
