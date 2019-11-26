8a78afcaed0497199bda7aacd56428e97dc43443
carlos henrique <carlos.hrq.rodrigues@gmail.com>
2019-11-26 20:22:54 -0300

    dados projeto

---
+++ SN_WebApi/Models/Usuario/UserSimple.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SN_WebApi.Models.Usuario {
+    public class UserSimple {
+
+        public Guid Id {
+            get; set;
+        }
+
+        public string Nome {
+            get; set;
+        }
+
+        public string Email {
+            get; set;
+        }
+    }
+}
