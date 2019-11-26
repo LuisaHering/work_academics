eeaed36e0d0791dcbd6680c00cc4e303f60a0288
gabriel <gabrielcouto26@gmail.com>
2019-11-26 16:21:23 -0300

    javascript é nosso pai e nada nos faltará

--- SN_WebMVC/Scripts/SideBar.js
+++ SN_WebMVC/Scripts/SideBar.js
@@ -1,36 +1,76 @@
-﻿var btn = document.getElementById('btn-resize')
+﻿// botoes de açao
+var btnDiminui = document.getElementById('btn-resize1')
+var btnCresce = document.getElementById('btn-resize2')
 
-var small = document.querySelectorAll(".wa-sidebar-list-small")
-var full = document.querySelectorAll(".wa-sidebar-list-full")
-var waSideBar = document.querySelector(".wa-sidebar")
+// elementos da sidebar
+var waSideBar = document.getElementById("wa-sidebar-id")
+var profilePic = document.querySelector(".wa-profile-pic")
+var perfilBtn = document.querySelector(".wa-perfil-btn")
+var listaTexto = document.querySelector(".wa-sidebar-list-grande")
+var listaIcones = document.querySelector(".wa-sidebar-list-pequeno")
+var waHr = document.querySelector(".wa-hr")
 var miniLogo = document.getElementById("mini-logo-sidebar")
 
+// body 
+var bodyContent = document.querySelector(".body-content")
 
-btn.addEventListener('click', function () {
-
-    // cresce
-    for (var i = 0; i < small.length; i++) {
-        if (small[i].style.display == "none") {
-            small[i].style.display = "block"
-            waSideBar.style.width = "6em"
-            miniLogo.style.margin = "0"
-            btn.style.marginBottom = "20px"
-        } else {
-            small[i].style.display = "none"
-            waSideBar.style.width = "12em"
-            miniLogo.style.margin = "20px 35px"
-            btn.style.marginBottom = "0"
-        }
-    }
+btnDiminui.addEventListener('click', function (event) {
+    event.preventDefault();
 
     // diminui
-    for (var i = 0; i < full.length; i++) {
-        if (full[i].style.display == "block") {
-            full[i].style.display = "none"
-        } else {
-            full[i].style.display = "block"
-        }
-    }
+    waSideBar.classList.remove("wa-sidebar-grande")
+    waSideBar.classList.add("wa-sidebar-pequeno")
 
-    
+    profilePic.classList.remove("wa-profile-pic-grande")
+    profilePic.classList.add("wa-profile-pic-pequeno")
+
+    perfilBtn.classList.add("wa-perfil-btn-none")
+
+    listaTexto.style.display = "none"
+    listaIcones.style.display = "block"
+
+    waHr.classList.remove("wa-hr-grande")
+    waHr.classList.add("wa-hr-pequeno")
+
+    miniLogo.classList.remove("wa-mini-logo-grande")
+    miniLogo.classList.add("wa-mini-logo-pequeno")
+
+    bodyContent.classList.remove("wa-body-content-margin12")
+    bodyContent.classList.add("wa-body-content-margin6")
+
+    btnDiminui.classList.add("wa-resize-btn-none")
+
+    btnCresce.classList.remove("wa-resize-btn-none")
+
+})
+
+
+btnCresce.addEventListener('click', function (event) {
+    event.preventDefault();
+
+    // cresce
+    waSideBar.classList.remove("wa-sidebar-pequeno")
+    waSideBar.classList.add("wa-sidebar-grande")
+
+    profilePic.classList.remove("wa-profile-pic-pequeno")
+    profilePic.classList.add("wa-profile-pic-grande")
+
+    perfilBtn.classList.remove("wa-perfil-btn-none")
+
+    listaTexto.style.display = "block"
+    listaIcones.style.display = "none"
+
+    waHr.classList.remove("wa-hr-pequeno")
+    waHr.classList.add("wa-hr-grande")
+
+    miniLogo.classList.remove("wa-mini-logo-pequeno")
+    miniLogo.classList.add("wa-mini-logo-grande")
+
+    bodyContent.classList.remove("wa-body-content-margin6")
+    bodyContent.classList.add("wa-body-content-margin12")
+
+    btnCresce.classList.add("wa-resize-btn-none")
+
+    btnDiminui.classList.remove("wa-resize-btn-none")
+
 })
