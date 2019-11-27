3469c104406ecb8298b13938f6c7dc43188dbfa9
gabriel <gabrielcouto26@gmail.com>
2019-11-27 03:08:40 -0300

    iniciando nightmode

---
+++ SN_WebMVC/Scripts/Nightmode.js
@@ -0,0 +1,32 @@
+﻿var btn_nightmode = document.getElementById("btn-nightmode")
+var btn_lightmode = document.getElementById("btn-lightmode")
+
+var bodyContent = document.querySelector(".body-content")
+var container = document.querySelector(".container")
+var sidebar = document.querySelector(".wa-sidebar")
+var navbar = document.querySelector(".navbar")
+
+
+btn_nightmode.addEventListener('click', function () {
+
+    bodyContent.classList.add('nightmode')
+
+    btn_nightmode.classList.add('btn-nightmode-none')
+    btn_lightmode.classList.remove('btn-nightmode-none')
+
+    sidebar.classList.add("nightmode")
+    navbar.classList.add("navbar-nightmode")
+
+})
+
+btn_lightmode.addEventListener('click', function () {
+
+    bodyContent.classList.remove('nightmode')
+
+    btn_lightmode.classList.add('btn-nightmode-none')
+    btn_nightmode.classList.remove('btn-nightmode-none')
+
+    sidebar.classList.remove("nightmode")
+    navbar.classList.remove("navbar-nightmode")
+
+})
