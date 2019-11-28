5636baf859e6339b95d6d247199dbe27de249540
Gabriel de Lima Couto <gabriel.couto@al.infnet.edu.br>
2019-11-27 21:00:38 -0300

    criando postviewmodel

---
+++ SN_WebMVC/Models/PostViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SN_WebMVC.Models
+{
+    public class PostViewModel
+    {
+        public string Id { get; set; }
+        public string Autor { get; set; }
+        public DateTime DataDePublicacao { get; set; }
+        public string Mensagem { get; set; }
+        public string UrlDocumento { get; set; }
+
+    }
+}
