5fe8e04b44bbcfbeed8fa430993b6b0cd5dae4bb
carlos henrique <carlos.hrq.rodrigues@gmail.com>
2019-11-28 15:51:13 -0300

    mock

--- SN_WebMVC/Controllers/ProjectController.cs
+++ SN_WebMVC/Controllers/ProjectController.cs
@@ -85,7 +85,45 @@ namespace SN_WebMVC.Controllers {
 
         [HttpGet]
         public async Task<ActionResult> Home(int id) {
+            var laboratory = new LaboratoryViewModel();
+            laboratory.Id = 1;
+            laboratory.Descricao = "Descricao do laboratorio";
+
+            var membro1 = new MembrosOutputModel();
+            membro1.Id = "123";
+            membro1.Nome = "Membro 1";
+            membro1.Email = "membro@membro.com";
+
+            var membro2 = new MembrosOutputModel();
+            membro2.Id = "321";
+            membro2.Nome = "Membro 2";
+            membro2.Email = "membro2@membro2.com";
+
             var projeto = new ProjetoOutputModel();
+            projeto.Id = "123456";
+            projeto.Titulo = "Titulo do projeto";
+            projeto.Descricao = "Descricao do projeto";
+            projeto.DataCriacao = DateTime.Now;
+            projeto.laboratory = laboratory;
+            projeto.Membros.Add(membro1);
+            projeto.Membros.Add(membro2);
+
+            projeto.laboratory = laboratory;
+
+            var post = new PostViewModel();
+            post.Id = "abc123";
+            post.Mensagem = "mensagem 1";
+            post.DataDePublicacao = DateTime.Now;
+            post.Autor = "auto1";
+
+            var post2 = new PostViewModel();
+            post2.Id = "abc123";
+            post2.Mensagem = "mensagem 1";
+            post2.DataDePublicacao = DateTime.Now;
+            post2.Autor = "auto2";
+
+            projeto.Posts.Add(post);
+            projeto.Posts.Add(post2);
 
             using(var client = new HttpClient()) {
                 client.BaseAddress = new Uri(base_url);
@@ -93,7 +131,9 @@ namespace SN_WebMVC.Controllers {
 
                 if(response.IsSuccessStatusCode) {
                     var responseContent = await response.Content.ReadAsStringAsync();
-                    projeto = JsonConvert.DeserializeObject<ProjetoOutputModel>(responseContent);
+
+                    //DESCOMENTAR QUANDO ESTIVER CADASTRANDO OS POSTS
+                    // projeto = JsonConvert.DeserializeObject<ProjetoOutputModel>(responseContent);
                     return View(projeto);
                 }
             }
